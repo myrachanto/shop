7c5a599982752c4f0e9e4bd8ca5db06f26a2414b
myrachanto <myrachanto@gmail.com>
2021-09-22 07:58:43 +0300

    data change

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,40 +1,19 @@
 export const data = [
-  {'name': 'home'}, 
-  {'name':'all-cakes'},
-  {'name':'wedding-cakes'},
-  {'name':'birthday-cakes'},
-  {'name':'fruit-cakes'},
-  {'name':'themed-cakes'},
-  {'name':'metropolitan-cakes'},
-  {'name':'cupcakes'},
-  {'name': 'others'},
-  {'name': 'fruit'},
-  {'name': 'chocolate'},
-  {'name': 'vanilla'},
-  {'name': 'velvet'},
-  {'name': 'forest'},
-  {'name': 'metropolitan'},
-  {'name': 'themed'},
-  {'name':'banana'},
-  {'name':'strawberry'},
-  {'name':'orange'},
-  {'name':'lemon'},
-  {'name':'passion'},
-  {'name':'other'},
-  {'name':'red'},
-  {'name':'purple'},
-  {'name':'black'},
-  {'name':'white'},
-  {'name':'sofia'},
-  {'name':'elsa'},
-  {'name':'cocolmellon'},
-  {'name':'pjmask'},
-  {'name':'spiderman'},
+  {'name': 'home'},
+  {'name': 'subsection'},
   {'name': 'aboutus'},
   {'name': 'contactus'},
   {'name': 'search'},
-  {'name': 'guide'},
   {'name': 'blog'},
+  {'name': 'men'},
+  {'name': 'women'},
+  {'name': 'children'},
+  {'name': 'hoodies'},
+  {'name': 'shirts'},
+  {'name': 't-shirts'},
+  {'name': 'jeans'},
+  {'name': 'khaki'},
+  {'name': 'shoes'},
   {'name': 'faq'},
   {'name': 'help'},
   {'name': 'support'},
@@ -42,63 +21,16 @@ export const data = [
   {'name': 'contract'},
   {'name': 'privacy'}
 ]
-export const supercategory = [
-  {'name': 'Fruit',
-    'category': [
-      {'name':'banana'},
-      {'name':'strawberry'},
-      {'name':'orange'},
-      {'name':'lemon'},
-      {'name':'passion'},
-      {'name':'other'}
-    ]  
-  },
-  {'name': 'chocolate',
-  'category': [
-    {'name':'other'}
-  ]  
-  },
-  {'name': 'vanilla',
-  'category': [
-    {'name':'other'}
-  ]  
-  },
-  {'name': 'velvet',
-  'category': [
-    {'name':'red'},
-    {'name':'purple'},
-    {'name':'other'}
-  ]  
-  },
-  {'name': 'forest',
-  'category': [
-    {'name':'black forest'},
-    {'name':'white forest'},
-    {'name':'other'}
-  ]  
-  },
-  {'name': 'metropolitan',
-  'category': [
-    {'name':'other'}
-  ]  
-  },
-  {'name': 'themed',
-  'category': [
-    {'name':'sofia'},
-    {'name':'elsa'},
-    {'name':'cocolmellon'},
-    {'name':'pjmask'},
-    {'name':'spiderman'},
-  ]  
-  }
+export const category = [
+  {'name': 'hoodies'},
+  {'name': 'shirts'},
+  {'name': 'jeans'},
+  {'name': 'khaki'},
+  {'name': 'shoes'},
+  {'name': 'dress'}
 ]
-export const majorcategory = [  
-  {'name':'wedding-cakes'},
-  {'name':'birthday-cakes'},
-  {'name':'fruit-cakes'},
-  {'name':'themed-cakes'},
-  {'name':'metropolitan-cakes'},
-  {'name':'cupcakes'},
-  {'name':'Other'}
+export const majorcategory = [
+  {'name': 'men'},
+  {'name': 'women'},
+  {'name': 'children'}
 ]
-
